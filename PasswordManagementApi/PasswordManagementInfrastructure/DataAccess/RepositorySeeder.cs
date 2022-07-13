1c7a828677174cbbcf55aaf30a130d2804b1965c
Dan Istoc <dan.c.istoc@gmail.com>
2022-07-14 01:50:10 +0300

    move to infrastructure project

--- PasswordManagementApi/PasswordManagementInfrastructure/DataAccess/RepositorySeeder.cs
+++ PasswordManagementApi/PasswordManagementInfrastructure/DataAccess/RepositorySeeder.cs
@@ -18,22 +18,29 @@ namespace PasswordManagementApi.Infrastructure.DataAccess
         {
             AddExpiredPasswords();
             AddValidPassword();
+            AddEmptyUsers();
         }
 
         private void AddExpiredPasswords()
         {
-            var uid = repository.AddUser("JohnSmith");
+            var uid = repository.AddUser("John_Smith");
             var pwd = pwdGenerator.Generate();
             repository.UpdatePassword(uid, pwd, DateTime.Now.AddDays(-1), DateTime.Now.AddDays(-1).AddSeconds(30));
 
-            uid = repository.AddUser("FooBar");
+            uid = repository.AddUser("Foo_Bar");
             pwd = pwdGenerator.Generate();
             repository.UpdatePassword(uid, pwd, DateTime.Now.AddHours(-1), DateTime.Now.AddHours(-1).AddSeconds(30));
         }
 
+        private void AddEmptyUsers()
+        {
+            repository.AddUser("Lola");
+            repository.AddUser("Someone_007");
+        }
+
         private void AddValidPassword()
         {
-            int uid = repository.AddUser("JohnSnow");
+            int uid = repository.AddUser("John_Snow");
             var pwd = pwdGenerator.Generate();
             repository.UpdatePassword(uid, pwd, DateTime.Now, DateTime.Now.AddHours(1));
         }
